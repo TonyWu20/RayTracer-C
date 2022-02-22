453bfdca35a220590dd1d6d0ba5c0ca247270f82
TonyWu20 <tony.w21@gmail.com>
2022-02-22 13:40:19 +0800

    Implement `PreComp`, `shade_hit`, `color_at`; fix bug when even if no intersect happens, func `intersect_world` still try to sort the collection

--- src/world/world.h
+++ src/world/world.h
@@ -1,17 +1,42 @@
 #pragma once
+#include <colors/colors.h>
 #include <geometry/geometry.h>
 #include <lights/lights.h>
 #include <ray/ray.h>
+#include <stdbool.h>
+
+/* typedef section */
 typedef struct world World;
+typedef struct PreComp PreComp;
+/* typedef section ends */
+
 struct world
 {
-    Light *light;
+    Light **lights;
+    int lightCounts;
     Sphere **sphereArray; // Pointer to arrays of Sphere pointer
     int sphereCounts;
-    void (*func_destroy)(World *self);
-    IntersectCollection *(*func_intersect_world)(World *self, Ray *rayPtr);
+    struct world_vtable *funcTab;
 };
 
+struct PreComp
+{
+    float t;
+    const Sphere *object;
+    simd_float4 *point;
+    simd_float4 *eyeV;
+    simd_float4 *normalV;
+    bool inside;
+    void (*destroy)(PreComp *self);
+};
+struct world_vtable
+{
+    void (*destroy)(World *self);
+    IntersectCollection *(*intersect_world)(World *self, Ray *rayPtr);
+    Color (*shade_hit)(World *self, PreComp *comp);
+    void (*add_light)(World *self, Light *light);
+    Color (*color_at)(World *self, Ray *r);
+};
 /* @abstract: Init a default world with a lightsource at (-10, -10, -10)
  * and two spheres at the origin, one is a unit sphere and another one in the
  * inner has a radius of 0.5
@@ -19,18 +44,40 @@ struct world
 static inline World *init_default_world(void);
 /* @abstract: Release all memory in the World instance */
 static inline void destroy_world(World *self);
+/* @abstract: Shading an intersection */
+/* @params: World *self, PreComp *comp */
+static inline Color shade_hit(World *self, PreComp *comp);
+/* @abstract: Add a light source to the world, update lightCounts by one */
+/* @params: World *self, Light *light */
+static inline void add_light(World *self, Light *light);
+/* @abstract: Intersect the world with the given ray, and then return
+ * the color at the intersection */
+static inline Color color_at(World *self, Ray *r);
+/* @abstrat: Release memory of a PreComp struct */
+/* @abstract: Init a PreComp struct by an Intersect and a ray */
+/* @params: const SphereIntersect *, const Ray * */
+/* @Warning: Free after use! */
+static inline PreComp *prepare_computations(const SphereIntersect *intxs,
+                                            const Ray *r);
+static inline void destroy_precomp(PreComp *self);
 /* @abstrac: Returns the IntersectCollection ptr of a ray intersects with
  * the world.
- */
+ * @Warning: The return IntersectCollection does not need to free immediately.
+ * It will be freed when the Ray * is destroyed. */
 static inline IntersectCollection *intersect_world(World *self, Ray *rayPtr);
 
 #pragma mark -Implementation
+struct world_vtable WorldVtable = {
+    destroy_world, intersect_world, shade_hit, add_light, color_at,
+};
 static inline World *init_default_world(void)
 {
     World *new = malloc(sizeof(World));
+    new->lights = malloc(sizeof(Light *));
     Light *light = malloc(sizeof(Light));
     *light = point_light((simd_float4){-10, -10, -10, 1}, (Color){1, 1, 1});
-    new->light = light;
+    new->lights[0] = light;
+    new->lightCounts = 1;
     new->sphereArray = malloc(2 * sizeof(Sphere *));
     for (int i = 0; i < 2; ++i)
     {
@@ -44,13 +91,16 @@ static inline World *init_default_world(void)
     s1->m.diffuse = 0.7;
     s1->m.specular = 0.2;
     new->sphereCounts = 2;
-    new->func_destroy = destroy_world;
-    new->func_intersect_world = intersect_world;
+    new->funcTab = &WorldVtable;
     return new;
 }
 static inline void destroy_world(World *self)
 {
-    free(self->light);
+    for (int i = 0; i < self->lightCounts; ++i)
+    {
+        free(self->lights[i]);
+    }
+    free(self->lights);
     for (int i = 0; i < self->sphereCounts; ++i)
     {
         free(self->sphereArray[i]);
@@ -58,12 +108,74 @@ static inline void destroy_world(World *self)
     free(self->sphereArray);
     free(self);
 }
+static inline void add_light(World *self, Light *light)
+{
+    self->lightCounts++;
+    self->lights = realloc(self->lights, self->lightCounts * sizeof(Light *));
+    self->lights[self->lightCounts - 1] = malloc(sizeof(Light));
+    *(self->lights[self->lightCounts - 1]) = *light;
+}
+static inline Color color_at(World *self, Ray *r)
+{
+    IntersectCollection *xs = self->funcTab->intersect_world(self, r);
+    if (!xs)
+        return (Color){0, 0, 0};
+    SphereIntersect *hit = hit_Sphere(xs);
+    PreComp *comp = prepare_computations(hit, r);
+    Color result = shade_hit(self, comp);
+    comp->destroy(comp);
+    return result;
+}
+static inline PreComp *prepare_computations(const SphereIntersect *intxs,
+                                            const Ray *r)
+{
+    PreComp *comp = malloc(sizeof(PreComp));
+    comp->t = intxs->t;
+    comp->object = intxs->object;
+    comp->point = malloc(sizeof(simd_float4));
+    *comp->point = currPosition(r, intxs->t);
+    comp->eyeV = malloc(sizeof(simd_float4));
+    *comp->eyeV = simd_make_float4(simd_make_float3(-r->directionVec), 0);
+    comp->normalV = malloc(sizeof(simd_float4));
+    *comp->normalV =
+        surface_normal_at(intxs->object, (simd_float4 *)comp->point);
+    if (simd_dot(*comp->normalV, *comp->eyeV) < 0)
+    {
+        comp->inside = true;
+        *comp->normalV = -1 * *(comp->normalV);
+        *comp->normalV = simd_make_float4(simd_make_float3(*comp->normalV), 0);
+    }
+    else
+    {
+        comp->inside = false;
+    }
+    comp->destroy = destroy_precomp;
+    return comp;
+}
+static inline void destroy_precomp(PreComp *self)
+{
+    free(self->point);
+    free(self->eyeV);
+    free(self->normalV);
+    free(self);
+}
+static inline Color shade_hit(World *self, PreComp *comp)
+{
+    Color result = {0, 0, 0};
+    for (int i = 0; i < self->lightCounts; ++i)
+    {
+        result += lighting(&comp->object->m, self->lights[i], comp->point,
+                           comp->eyeV, comp->normalV);
+    }
+    return result;
+}
 static inline IntersectCollection *intersect_world(World *self, Ray *rayPtr)
 {
     for (int i = 0; i < self->sphereCounts; ++i)
     {
         rayPtr->intersects_with_Sphere(rayPtr, self->sphereArray[i]);
     }
-    sort_xs(rayPtr->xs);
+    if (rayPtr->xs)
+        sort_xs(rayPtr->xs);
     return rayPtr->xs;
 }
--- src/world_test.c
+++ src/world_test.c
@@ -3,15 +3,22 @@
 static void test_world()
 {
     World *w = init_default_world();
-    Ray r = init_Ray((simd_float4){0, 0, -5, 1}, (simd_float4){0, 0, 1, 0});
-    IntersectCollection *xs = w->func_intersect_world(w, &r);
-    printf("Intersects count: %d\n", xs->intersects_counts);
-    for (int i = 0; i < xs->intersects_counts; ++i)
-    {
-        printf("xs[%d].t = %f\n", i, xs->intersects[i]->pos.z);
-    }
+    Ray r = init_Ray((simd_float4){0, 0, -5, 1}, (simd_float4){0, 1, 0, 0});
+    printf("Color when a ray misses ");
+    print_float3(color_at(w, &r));
     r.destroy_XS(&r);
-    w->func_destroy(w);
+    r.directionVec = (simd_float4){0, 0, 1, 0};
+    printf("Color when a ray hits ");
+    print_float3(color_at(w, &r));
+    r.destroy_XS(&r);
+    w->sphereArray[0]->m.ambient = 1;
+    w->sphereArray[1]->m.ambient = 1;
+    r = init_Ray((simd_float4){0, 0, 0.75, 1}, (simd_float4){0, 0, -1, 0});
+    printf("Color with an intersection behind the ray ");
+    print_float3(color_at(w, &r));
+    print_float3(w->sphereArray[1]->m.color);
+    r.destroy_XS(&r);
+    w->funcTab->destroy(w);
 }
 
 int main(int argc, char *argv[])
