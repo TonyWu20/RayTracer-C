95baf892a5c39ed88eea2c411cce26c3877b14f8
TonyWu20 <tony.w21@gmail.com>
2022-02-24 03:17:54 +0800

    Implement ansi true color output

--- src/canvas/canvas.h
+++ src/canvas/canvas.h
@@ -1,4 +1,7 @@
+#pragma once
+#include <colors/colors.h>
 #include <stdio.h>
+#include <simd/simd.h>
 #include <stdlib.h>
 #include <string.h>
 typedef struct _Canvas Canvas;
@@ -7,11 +10,10 @@ struct _Canvas
     int width;
     int height;
     int pixelNums;
-    char *pixels;
-    void (*writePixel)(Canvas *self, int x, int y, char symbol);
-    void (*writeLumaPixel)(Canvas *self, int x, int y, int luma);
+    char **pixels;
+    void (*writeLumaPixel)(Canvas *self, int x, int y, Color pixel);
     void (*show)(Canvas *self);
-    void (*clear)(Canvas *self);
+    /* void (*clear)(Canvas *self); */
     void (*destroy)(Canvas *self);
 };
 /* @abstract: Init a pointer of Canvas by specifiying the width and height */
@@ -19,7 +21,7 @@ static inline Canvas *init_Canvas(int width, int height);
 /* @abstract: Write pixel to Canvas buffer */
 static inline void writeToCanvas(Canvas *self, int x, int y, char symbol);
 /* @abstract: Write pixel determined by the luma value to Canvas buffer */
-static inline void writeToCanvas_luma(Canvas *self, int x, int y, int luma);
+static inline void writeToCanvas_luma(Canvas *self, int x, int y, Color pixel);
 /* @abstract: Print the content of Canvas to terminal */
 static inline void printCanvas(Canvas *self);
 /* @abstract: Clear the content of Canvas to terminal */
@@ -30,36 +32,37 @@ static inline void destroyCanvas(Canvas *self);
 /* @abstract: Init a pointer of Canvas by specifiying the width and height */
 static inline Canvas *init_Canvas(int width, int height)
 {
-    Canvas *cPtr = malloc(sizeof(Canvas));
-    cPtr->width = width;
-    cPtr->height = height;
-    cPtr->pixelNums = width * height;
-    cPtr->pixels = malloc(width * height * sizeof(char));
-    memset(cPtr->pixels, ' ', width * height);
-    cPtr->writePixel = writeToCanvas;
-    cPtr->writeLumaPixel = writeToCanvas_luma;
-    cPtr->show = printCanvas;
-    cPtr->clear = clearCanvas;
-    cPtr->destroy = destroyCanvas;
-    return cPtr;
+    Canvas *self = malloc(sizeof(Canvas));
+    self->width = width;
+    self->height = height;
+    self->pixelNums = width * height;
+    self->pixels = malloc(width * height * sizeof(char *));
+    for (int i = 0; i < self->pixelNums; ++i)
+    {
+        self->pixels[i]= malloc(sizeof(char)*48);
+        snprintf(self->pixels[i], 2, " ");
+    }
+    self->writeLumaPixel = writeToCanvas_luma;
+    self->show = printCanvas;
+    /* self->clear = clearCanvas; */
+    self->destroy = destroyCanvas;
+    return self;
 }
-static inline void writeToCanvas(Canvas *self, int x, int y, char symbol)
+static inline void writeToCanvas_luma(Canvas *self, int x, int y, Color pixel)
 {
     if (x < 0 || x >= self->width || y < 0 || y >= self->height)
     {
         return;
     }
+    pixel = simd_clamp(pixel, 0, 1);
+    char symbol = lumaToChar(pixel);
+    pixel = 255 * pixel;
     int Y = self->height - 1 - y;
-    self->pixels[Y * self->width + x] = symbol;
-}
-static inline void writeToCanvas_luma(Canvas *self, int x, int y, int luma)
-{
-    if (x < 0 || x >= self->width || y < 0 || y >= self->height)
-    {
-        return;
-    }
-    int Y = self->height - 1 - y;
-    self->pixels[Y * self->width + x] = ".,-~:;=!*#$@"[luma > 0 ? luma : 0];
+    char format[] = "\x1b[38;2;%d;%d;%dm%c\x1b[0m";
+    int sLen = 1 + snprintf(NULL, 0, format, (int)pixel.x, (int)pixel.y, (int)pixel.z, symbol);
+    int idx = Y * self->width + x;
+    snprintf(self->pixels[idx], sLen, format, (int)pixel.x, (int)pixel.y, (int)pixel.z, symbol);
+    /* self->pixels[Y * self->width + x] = ".,-~:;=!*#$@"[luma > 0 ? luma : 0]; */
 }
 static inline void clearCanvas(Canvas *self)
 {
@@ -68,14 +71,18 @@ static inline void clearCanvas(Canvas *self)
 }
 static inline void destroyCanvas(Canvas *self)
 {
+    for (int i = 0; i < self->pixelNums; ++i)
+    {
+        free(self->pixels[i]);
+    }
     free(self->pixels);
     free(self);
 }
 static inline void printCanvas(Canvas *self)
 {
-    printf("\x1b[2J\x1b[H"); // Clear screen
+    printf("\x1b[2J""\x1b[H"); // Clear screen
     for (int i = 0; i < self->pixelNums; ++i)
     {
-        putchar((i % self->width) ? self->pixels[i] : 10);
+        printf("%s", (i % self->width) ? self->pixels[i] : "\n");
     }
 }
