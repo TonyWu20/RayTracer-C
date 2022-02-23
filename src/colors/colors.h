7764f65c3c271a1a13f7dd49005a4a03bce997c8
TonyWu20 <tony.w21@gmail.com>
2022-02-24 03:18:54 +0800

    New function `lumaToChar` to determine the char to use

--- src/colors/colors.h
+++ src/colors/colors.h
@@ -11,3 +11,12 @@ static inline int lumaForColor(Color result)
     int lumN = 11 * lum;
     return lumN;
 }
+static inline char lumaToChar(Color result)
+{
+    simd_float3 luminanceFactor = {0.2126, 0.7152, 0.0722};
+    result = simd_clamp(result, 0, 1);
+    float lum = simd_dot(luminanceFactor, result);
+    char grayscale[] = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'";
+    int lumN = 68 * lum;
+    return grayscale[68-lumN];
+}
