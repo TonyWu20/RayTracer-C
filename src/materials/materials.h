891ee5045270e46e397826255ff137d03551a6e9
TonyWu20 <tony.w21@gmail.com>
2022-02-26 02:40:53 +0800

    Add reflective (Chapter 11)

--- src/materials/materials.h
+++ src/materials/materials.h
@@ -1,8 +1,11 @@
 #pragma once
+// clang-format off
 #include <colors/colors.h>
+#include <types/common.h>
 #include <types/types.h>
+/* #include <geometry/patterns.h> */
+// clang-format on
 
-typedef struct Material Material;
 struct Material
 {
     Color color;
@@ -11,12 +14,15 @@ struct Material
     float specular;  // 0 < x <= 1
     float shininess; // usually 10 (large highlight) to 200 (very small) or
                      // higher val
+    float reflective;
+    Pattern *pattern;
 };
 
 /* @abstract: Create a material from input parameters */
 static inline Material create_material(Color color, float ambient,
                                        float diffuse, float specular,
-                                       float shininess);
+                                       float shininess, float reflective,
+                                       Pattern *pattern);
 /* @abstract: Returns a default Material
  * @returns: A Material
  */
@@ -25,9 +31,11 @@ static inline Material defaultMaterial(void);
 #pragma mark -Implementations
 static inline Material create_material(Color color, float ambient,
                                        float diffuse, float specular,
-                                       float shininess)
+                                       float shininess, float reflective,
+                                       Pattern *pattern)
 {
-    Material result = {color, ambient, diffuse, specular, shininess};
+    Material result = {color,     ambient,    diffuse, specular,
+                       shininess, reflective, pattern};
     return result;
 }
 static inline Material defaultMaterial(void)
@@ -37,7 +45,9 @@ static inline Material defaultMaterial(void)
     float diffuse = 0.9;
     float specular = 0.9;
     float shininess = 200;
-    Material result =
-        create_material(color, ambient, diffuse, specular, shininess);
+    float reflective = 0.0;
+    Pattern *pattern = NULL;
+    Material result = create_material(color, ambient, diffuse, specular,
+                                      shininess, reflective, pattern);
     return result;
 }
